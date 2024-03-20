6bac248b29b7cad403c2262529c20ed229e4cb47
Adarsh Kumar Sahoo <91600390+Adarshsahoo1703@users.noreply.github.com>
2024-03-20 09:15:44 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of nodes on the longest path from root to leaf node/sum-of-nodes-on-the-longest-path-from-root-to-leaf-node.java
@@ -0,0 +1,143 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*; 
+
+
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+
+class GFG {
+    
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    public static void inorder(Node root){
+        if(root == null)
+            return;
+        
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+		while(t-- > 0){
+		    String s = br.readLine();
+	    	Node root = buildTree(s);
+		    Solution obj = new Solution();
+		    int res = obj.sumOfLongRootToLeafPath(root);
+		    System.out.println(res);
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+node class of binary tree
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+*/
+class Solution {
+    int maxSum = Integer.MIN_VALUE;
+    int maxPathLength = 0;
+
+    public int sumOfLongRootToLeafPath(Node root) {
+        if (root == null)
+            return 0;
+        
+        dfs(root, root.data, 1); // Start with depth 1
+        
+        return maxSum;
+    }
+
+    private void dfs(Node node, int pathSum, int depth) {
+        if (node.left == null && node.right == null) {
+            // Update the maximum sum and path length if a longer path is found
+            if (depth > maxPathLength || (depth == maxPathLength && pathSum > maxSum)) {
+                maxPathLength = depth;
+                maxSum = pathSum;
+            }
+            return;
+        }
+
+        if (node.left != null)
+            dfs(node.left, pathSum + node.left.data, depth + 1);
+        
+        if (node.right != null)
+            dfs(node.right, pathSum + node.right.data, depth + 1);
+    }
+}
