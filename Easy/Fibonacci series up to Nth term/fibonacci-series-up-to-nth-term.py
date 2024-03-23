95c837bedccff9f2e71cecf0fea9f3fa53d9484c
Adarsh Kumar Sahoo <91600390+Adarshsahoo1703@users.noreply.github.com>
2024-03-23 09:30:42 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Fibonacci series up to Nth term/fibonacci-series-up-to-nth-term.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+
+class Solution:
+    def series(self, n):
+        # Code here
+        a = 0
+        b =1 
+        fibo = []
+        fibo.append(a)
+        fibo.append(b)
+        for i in range(0,n-1):
+            c = fibo[i]+fibo[i+1]
+            fibo.append(c%(10**9+7))
+            
+        return fibo  
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        result = ob.series(N)
+        print(*result)
+# } Driver Code Ends
