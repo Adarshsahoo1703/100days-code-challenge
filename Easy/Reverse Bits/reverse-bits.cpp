a945c92779954002996fc1c2da0845c932cf65de
Adarsh Kumar Sahoo <91600390+Adarshsahoo1703@users.noreply.github.com>
2024-04-13 12:51:14 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse Bits/reverse-bits.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long reversedBits(long long x) {
+        // code here
+        long long sum=0, i=31;
+        while(x)
+        {
+            long long temp=x&1;
+            sum=sum+pow(2,i)*temp;
+            i--;
+            x=x>>1;
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long X;
+        
+        cin>>X;
+
+        Solution ob;
+        cout << ob.reversedBits(X) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
