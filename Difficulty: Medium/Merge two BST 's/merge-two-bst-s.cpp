d2629f22e8f414185213c67342031adc93f5bf25
Adarsh Kumar Sahoo <91600390+Adarshsahoo1703@users.noreply.github.com>
2024-09-30 21:33:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two BST 's/merge-two-bst-s.cpp
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.cpp
@@ -93,56 +93,29 @@ struct Node {
 };
 */
 class Solution {
+  private:
+    void dfs(vector<int>& v, Node *node)
+    {
+        if(node->left)
+        {
+            dfs(v, node->left);
+        }
+        v.push_back(node->data);
+        if(node->right)
+        {
+            dfs(v, node->right);
+        }
+    }
   public:
-  
-    // code to perforn in-order traversal of BSTs and form two sorted arrays
-    
-    void inOrderTraversal(Node* root, vector<int>& ans) {
-        stack<Node*> s;
-        Node* temp = root;
-    
-        while (!s.empty() || temp!=NULL) {
-            while (temp!=NULL) {
-                s.push(temp);
-                temp = temp->left;
-        }
-        temp = s.top();
-        s.pop();
-        ans.push_back(temp->data);
-        temp = temp->right;
-    }
-}
-    // code to merge two sorted vector using merge sort
-    
-    vector<int> MergeSortedVectors(vector<int>& v1,vector<int>& v2) {
-        vector<int> mergedArr;
-        int i = 0, j = 0;
-        while (i < v1.size() && j < v2.size()) {
-            if (v1[i] < v2[j]) {
-                mergedArr.push_back(v1[i]);
-                i++;
-            } 
-            else {
-                mergedArr.push_back(v2[j]);
-                j++;
-            }
-        }
-        while (i < v1.size()) {
-            mergedArr.push_back(v1[i]);
-            i++;
-        }
-        while (j < v2.size()) {
-            mergedArr.push_back(v2[j]);
-            j++;
-        }
-        return mergedArr;
-    }
-
+    // Function to return a list of integers denoting the node
+    // values of both the BST in a sorted order.
     vector<int> merge(Node *root1, Node *root2) {
-        vector<int> v1, v2;
-        inOrderTraversal(root1, v1);
-        inOrderTraversal(root2, v2);
-        return MergeSortedVectors(v1, v2);
+        // Your code here
+        vector<int> v;
+        dfs(v, root1);
+        dfs(v, root2);
+        sort(v.begin(), v.end());
+        return v;
     }
 };
 
